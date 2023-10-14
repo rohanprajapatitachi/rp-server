326421b2f57562179814972ae9f98c395af9ed2f
rohanprajapati <rohanprajapati369@gmail.com>
2023-10-14 21:26:51 +0545

    server setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express') // This is the first step to create server import or require express.
+
+const app = express()  // express started
+
+
+/*
+    / bhaneko root ho banesii main file run hunxa yo path maa
+    req ~~~> request ho  ~~~~> server lai maagxau 
+    res ~~~> response ho ~~~~> server return somthing
+*/
+
+app.get('/',function (req, res) {   
+    res.send('Hello World')
+})
+
+app.listen(3000, ()=> {
+    console.log("Server started sucessfully")
+})
---
+++ steps.js
@@ -0,0 +1,10 @@
+// npm ~~~~~> package manager ~~~~~> node package manager ~~~~> node required packages will be avaliable here
+// package.json ~~~~~> packages install ~~~> list
+
+
+// to create a server in javascript we will require a package named express.js
+// to start a backend project we need to type a command in terminal called npm init -y
+// how we will know project strated so it will create a package.json automatically
+// terminal  maa garyera npm run start
+// to install express we neeed a commmand npm install express
+
