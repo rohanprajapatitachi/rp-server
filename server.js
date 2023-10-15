ade87692e52aad614f656ac09c9e485566989d2f
sanjayguwaju <sanjayguwaju@gmail.com>
2023-10-15 22:51:42 +0545

    mongoose connect

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express') // This is the first step to create server import or require express.
+const mongoose = require('mongoose')
 
 const app = express()  // express started
 
@@ -6,7 +7,19 @@ const app = express()  // express started
     / bhaneko root ho banesii main file run hunxa yo path maa
     req ~~~> request ho  ~~~~> server lai maagxau 
     res ~~~> response ho ~~~~> server return somthing
+    
 */
+
+// Connect to database
+mongoose.connect("", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB Connection Successfull!'))
+    .catch((err) => {
+        console.log("DB Connection Error :", err);
+    });
+
 
 app.get('/',function (req, res) {   
     res.send('Hello World');
